8280d2520e4e2fe561537b0e0f900907ea538b0d
Glauko Pacheco <glaukopacheco@gmail.com>
2022-11-18 16:45:42 -0300

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useEffect } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  useEffect(() => {
+    const cursor = document.querySelector('#cursor')
+
+    window.addEventListener('mousemove',(e) => {
+      cursor.style.left = (e.x - 150) + 'px'
+      cursor.style.top = (e.y - 150) + 'px'
+    })
+  })
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <div className="container">
+        <div className="content">
+          <div className="content__text">
+            <h1 className="title">Revelando texto</h1>
+            <h1 className="subtitle">O apredizado é como o horizonte: não há limites.</h1>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+
+      <div className="cursor" id="cursor"></div>
+    </>
   )
 }
 
